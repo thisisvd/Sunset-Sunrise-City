980cba67e1c18218d3acf12db5a8961d4083a9c3
Vimal <vimaldubey123@outlook.com>
2021-09-09 23:39:04 +0530

    Rebuilding crashed Okhttp via Java to Kotlin...

---
+++ app/src/main/java/com/vdcodeassociate/sunsetsunrisecity/SunriseSunsetActivity.kt
@@ -0,0 +1,17 @@
+package com.vdcodeassociate.sunsetsunrisecity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SunriseSunsetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sunrise_sunset)
+    }
+
+    fun processData(view : View){
+
+    }
+
+}
